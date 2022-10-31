171fa4f03e2ac9d89342c7b71568ed734764bc86
Ethan Hubbartt <33075924+ehubbartt@users.noreply.github.com>
2022-10-30 19:08:42 -0700

    finsiihedf

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let icon = document.getElementsByClassName("icon")[0];
+let icon = document.querySelector(".icon");
 
-icon.addEventListener('click', responsive_control);
+icon.addEventListener("click", responsive_control);
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsive_control() {
@@ -10,4 +10,50 @@ function responsive_control() {
   } else {
     x.className = "topnav";
   }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
 }
